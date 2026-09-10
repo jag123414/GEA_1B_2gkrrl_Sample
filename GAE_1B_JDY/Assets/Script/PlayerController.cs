e67f6f3c0b5a56e9d44a692943c272cd4e261c2b
jag123414 <a01040264560@gmail.com>
2026-09-10 15:18:39 +0900

    [Feat] GEA_1B_JDY10

---
+++ GAE_1B_JDY/Assets/Script/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpPower = 5f;
+    public float gravity = -20f;
+    private float verticalVelocity;
+
+    private Vector2 moveInput;
+    private CharacterController controller;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && controller.isGrounded)
+        {
+            verticalVelocity = jumpPower;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        if (controller.isGrounded && verticalVelocity < 0f)
+        {
+            verticalVelocity = -2f;
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+        move = move * moveSpeed;
+        move.y = verticalVelocity;
+
+        controller.Move(move * Time.deltaTime);
+
+    }
+
+}
